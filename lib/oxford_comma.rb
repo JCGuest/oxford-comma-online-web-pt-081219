2d66e983cf81af544d08aa25c6f4bc6f3a187088
John Guest <jcguest90@gmail.com>
2019-08-16 17:16:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,8 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
     array[-1].insert(0, "and ")
-  else
-   return array.join(", ") 
-  
   end
+   array.join(", ") 
 end
 
 #["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
